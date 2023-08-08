5fb91ed46f0b96a947b8fbf3da0def0673d4b4d0
Laura <62561398+ArualDev@users.noreply.github.com>
2023-08-08 22:12:06 +0200

    Add alternative image resolutions

--- src/downloadGetters/getImageDownloads.js
+++ src/downloadGetters/getImageDownloads.js
@@ -2,11 +2,34 @@ import DownloadInfoImage from "../classes/DownloadInfoImage";
 import { fetchFileSize } from "../helpers/utils";
 
 export default async function getImageDownloads(data) {
+
+    const getQualityString = (width, height) => `${width}x${height}`;
+    const downloads = []
+
+    if(!data?.preview?.images[0])
+        return downloads;
+
     const url = data.url;
-    const sourceInfo = data?.preview?.images[0]?.source;
-    const width = sourceInfo?.width;
-    const height = sourceInfo?.height;
+    const sourceInfo = data.preview.images[0].source;
+
     const fileSize = await fetchFileSize(url);
-    const quality = width && height ? `${width}x${height}` : null;
-    return [new DownloadInfoImage(url, data.filenamePrefix, quality, fileSize)];
+
+    const quality = sourceInfo.width && sourceInfo.height
+        ? getQualityString(sourceInfo.width, sourceInfo.height)
+        : null;
+
+    downloads.push(new DownloadInfoImage(url, data.filenamePrefix, quality, fileSize));
+
+    const alternativeQualitiesData = data.preview.images[0].resolutions.reverse();
+    for (const imageInfo of alternativeQualitiesData) {
+        const fileSize = await fetchFileSize(url);
+
+        const quality = imageInfo.width && imageInfo.height
+            ? getQualityString(imageInfo.width, imageInfo.height)
+            : null;
+
+        downloads.push(new DownloadInfoImage(url, data.filenamePrefix, quality, fileSize))
+    }
+
+    return downloads;
 }
