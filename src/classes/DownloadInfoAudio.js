fc922e9c219f232503eb51d38e21c909d4684749
Laura <62561398+ArualDev@users.noreply.github.com>
2023-08-09 12:04:12 +0200

    Set quality to audi info and include in file name

--- src/classes/DownloadInfoAudio.js
+++ src/classes/DownloadInfoAudio.js
@@ -4,10 +4,7 @@ export default class DownloadInfoAudio extends DownloadInfo {
     constructor(link, filenamePrefix = "audio", quality, fileSize) {
         super(link, filenamePrefix, quality, fileSize);
         this.contentType = "Audio";
+        this.quality = quality;
         this.fileExt = ".mp4";
-    }
-
-    getFullFileName() {
-        return `${this.filenamePrefix}-audio${this.fileExt}`
     }
 }
