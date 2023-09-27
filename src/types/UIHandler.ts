4503fe34272426bf8a6ba549f160cfd52ab0bfb0
Laura <62561398+ArualDev@users.noreply.github.com>
2023-09-28 00:09:46 +0200

    Update UIHandler.ts

--- src/types/UIHandler.ts
+++ src/types/UIHandler.ts
@@ -3,7 +3,11 @@ import type DownloadData from "./DownloadData";
 
 export default interface UIHandler {
     detectPosts: () => HTMLElement[];
-    injectDownloadButton: (post: Element, downloads: DownloadData[], onClick: (e: MouseEvent) => void) => void;
+    injectDownloadButton: (
+        post: Element, downloads: DownloadData[],
+        onClick: (e: MouseEvent) => void,
+        onClickMore: (e: MouseEvent) => void
+    ) => void;
     upvote: (post: HTMLElement) => void;
     getPostURL: (post: HTMLElement) => string;
     getDownloads: (post: HTMLElement, downloadType?: DownloadType) => Promise<DownloadData[]>;
