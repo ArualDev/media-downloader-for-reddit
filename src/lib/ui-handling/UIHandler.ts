796d0e7d2284e02baa07c6690611dcad3b2b6a37
Laura <62561398+ArualDev@users.noreply.github.com>
2023-12-02 22:33:27 +0100

    Change method declaration syntax for consistency

--- src/lib/ui-handling/UIHandler.ts
+++ src/lib/ui-handling/UIHandler.ts
@@ -5,16 +5,15 @@ import type { ImageDownloadable } from "../downloadable/ImageDownloadable";
 import type { VideoDownloadable } from "../downloadable/VideoDownloadable";
 
 export default interface UIHandler {
-    detectPosts: () => HTMLElement[];
-    injectDownloadButton: (
+    detectPosts(): HTMLElement[];
+    injectDownloadButton(
         post: Element, downloads: BaseDownloadable[],
         onClick: (e: MouseEvent) => void,
         onClickMore: (e: MouseEvent) => void
-    ) => void;
-    upvote: (post: HTMLElement) => void;
-    getPostURL: (post: HTMLElement) => string;
+    ): void;
+    upvote(post: HTMLElement): void;
+    getPostURL(post: HTMLElement): string;
     getPostPermalink(post: HTMLElement): string;
-    getPrimaryDownloadType: (post: HTMLElement) => DownloadType | null;
 
     getImageDownloadables(post: HTMLElement): Promise<ImageDownloadable[]>;
     getVideoDownloadables(post: HTMLElement): Promise<VideoDownloadable[]>;
